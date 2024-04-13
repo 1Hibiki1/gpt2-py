6aa219f877a791cad5953d9da6d90ff0ea48cb8c
hibiki <43519087+1Hibiki1@users.noreply.github.com>
2024-04-13 23:00:00 +0300

    ehhhhh

--- gpt2.py
+++ gpt2.py
@@ -2,11 +2,17 @@ import json
 
 import numpy as np
 import regex
+import tiktoken
 
 type Embedding = np.array
 type Token = str
 type Tensor = np.array
 
+def _layer_norm(x, g, b, eps: float = 1e-5):
+    mean = np.mean(x, axis=-1, keepdims=True)
+    variance = np.var(x, axis=-1, keepdims=True)
+    x = (x - mean) / np.sqrt(variance + eps)  # normalize x to have mean=0 and var=1 over last axis
+    return g * x + b  # scale and offset with gamma/beta params
 
 class Loader:
     def __init__(
@@ -29,10 +35,12 @@ class Loader:
         tensor_data_buf = self.safetensors_file.read(
             tensor_end_offset - tensor_start_offset
         )
-        tensot_np_array = np.frombuffer(tensor_data_buf, dtype="f")
-        tensot_np_array = tensot_np_array.reshape(tuple(tensor_meta["shape"]))
+        tensor_np_array = np.frombuffer(tensor_data_buf, dtype="f")
+        if len(tensor_meta["shape"]) == 1:
+            tensor_meta["shape"] = [1] + tensor_meta["shape"]
+        tensor_np_array = np.reshape(tensor_np_array, tensor_meta["shape"])
 
-        return tensot_np_array
+        return tensor_np_array
 
 
 class Tokenizer:
@@ -85,42 +93,10 @@ class Tokenizer:
             return ""
 
 
-class GPT2KVCache:
-    def __init__(self) -> None:
-        self.k_cache = list()
-        self.v_cache = list()
-
-        self.cur_idx = 0
-
-    def append_kv(self, kv_tuple) -> None:
-        self.k_cache.append(kv_tuple[0])
-        self.v_cache.append(kv_tuple[1])
-
-    def __iter__(self):
-        self.cur_idx = 0
-        return self
-
-    def __next__(self):
-        l = len(self.k_cache)
-        if self.cur_idx >= l:
-            raise StopIteration
-        kv = (self.k_cache[self.cur_idx], self.v_cache[self.cur_idx])
-        self.cur_idx += 1
-        return kv
-
-
 class GPT2Layer:
     def __init__(self) -> None:
-        self.q_weights: np.array = None
-        self.q_bias: np.array = None
-
-        self.k_weights: np.array = None
-        self.k_bias: np.array = None
-
-        self.v_weights: np.array = None
-        self.v_bias: np.array = None
-
-        self.kv_cache = GPT2KVCache()
+        self.attn_weights = None
+        self.attn_bias = None
 
         self.attn_proj_weights = None
         self.attn_proj_bias = None
@@ -147,65 +123,51 @@ class GPT2Layer:
     def _gelu(self, x):
         return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * x**3)))
 
-    def forward(self, input: Embedding) -> Embedding:
-        # 1) norm
-        norm = (input - np.mean(input)) / np.std(input)
-        norm = norm * self.ln_1_weights + self.ln_1_bias
+    def forward(self, input: list[Embedding]) -> list[Embedding]:
+        # norm
+        norm1 = _layer_norm(input, self.ln_1_weights, self.ln_1_bias)
+        
+        # attn (q,k,v)
+        qkv = np.dot(norm1, self.attn_weights)
+        qkv = np.add(qkv, self.attn_bias)
 
-        # 2) compute query?
-        query = np.dot(norm, self.q_weights) + self.q_bias
+        # self attn
+        q, k, v = np.split(qkv, 3, -1)
+        q_split = np.split(q, 12, -1)
+        k_split = np.split(k, 12, -1)
+        v_split = np.split(v, 12, -1)
 
-        # split attention head stuf
-        query = query.reshape((12, 64))
+        mask = (1 - np.tri(len(input))) * -1e10
 
-        output = list()
-        # 3) for all previous tokens, compute softmax(QK.t/sqrt(d))*V and append res to output
-        for k, v in self.kv_cache:
-            kq = query * k
-            kq = np.sum(kq, axis=1)
-            kq = kq / np.sqrt(768 / 12)
-            # TODO: mask kq? ugh what????
-            kq = self._softmax(kq)
+        out_split = list()
+        for i in range(12):
+            out_split.append(self._softmax(((q_split[i] @ k_split[i].T)/np.sqrt(q_split[i].shape[-1])) + mask) @ v_split[i])
 
-            v_scaled = (v.T * kq).T
+        attn_out = np.hstack(out_split)
 
-            final_v = v_scaled.reshape(768)
+        # proj
+        attn_proj = attn_out @ self.attn_proj_weights + self.attn_proj_bias
 
-            output.append(final_v)
+        # add input
+        inp_ffn = attn_proj + input
 
-        output = np.array(output)
-        # 4) proj
-        output = np.dot(output, self.attn_proj_weights) + self.attn_proj_bias
+        # FFN
 
-        # residual stufff
-        output += input
+        # norm 2
+        ffn_norm = _layer_norm(inp_ffn, self.ln_2_weights, self.ln_2_bias)
+        # mlp
+        ffn_out = (ffn_norm @ self.mlp_fc_weights) + self.mlp_fc_bias
+        ffn_out = self._gelu(ffn_out)
+        # proj
+        ffn_out = (ffn_out @ self.mlp_proj_weights) + self.mlp_proj_bias
 
-        # 5) norm 2
-        norm = (output - np.mean(output)) / np.std(output)
-        norm = norm * self.ln_2_weights + self.ln_2_bias
-
-        # 6) mlp
-        fc_out = np.dot(norm, self.mlp_fc_weights) + self.mlp_fc_bias
-        fc_out = self._gelu(fc_out)
-
-        proj_out = np.dot(fc_out, self.mlp_proj_weights) + self.mlp_proj_bias
-        proj_out = proj_out + output
-
-        # 7) "sum" all the outputs, and thats the input to the next layer (apparently)
-        return np.sum(proj_out, axis=0)
-
-    def cache_kv(self, input: Embedding) -> None:
-        k = np.dot(input, self.k_weights) + self.k_bias
-        v = np.dot(input, self.v_weights) + self.v_bias
-
-        # TODO: dont hardcode the shape lol
-        self.kv_cache.append_kv((k.reshape(12, 64), v.reshape(12, 64)))
+        return (ffn_out + inp_ffn)
 
 
 class GPT2:
     def __init__(self, dir: str = "model") -> None:
         self.tokenizer = Tokenizer(dir)
-        self.token_embedding_list = list()
+        self.encoder = tiktoken.get_encoding("r50k_base")
 
         self.loader = Loader()
         self.wte: np.array = self.loader.get_tensor("wte.weight")
@@ -222,12 +184,8 @@ class GPT2:
             qkv_weights = self.loader.get_tensor(f"h.{i}.attn.c_attn.weight")
             qkv_bias = self.loader.get_tensor(f"h.{i}.attn.c_attn.bias")
 
-            cur_layer.q_weights, cur_layer.k_weights, cur_layer.v_weights = np.split(
-                qkv_weights, 3, 1
-            )
-            cur_layer.q_bias, cur_layer.k_bias, cur_layer.v_bias = np.split(
-                qkv_bias, 3, 0
-            )
+            cur_layer.attn_weights = qkv_weights
+            cur_layer.attn_bias = qkv_bias
 
             cur_layer.attn_proj_weights = self.loader.get_tensor(
                 f"h.{i}.attn.c_proj.weight"
@@ -246,57 +204,41 @@ class GPT2:
 
             self.layers.append(cur_layer)
 
-    def prepare(self, prompt: str) -> None:
-        token_list = self.tokenizer.tokenize(prompt)
-        code_list = self.tokenizer.encode(token_list)
-
-        # get embeddings
-        self.token_embedding_list = np.take(self.wte, code_list, axis=0)
+    def get_next_token(self, prompt: str) -> str:
+        # encode
+        # token_list: list[int] = self.tokenizer.encode(self.tokenizer.tokenize(prompt))
+        token_list = self.encoder.encode(prompt)
+        # print()
+        # print(token_list)
 
         # wte + wpe
-        position_embeddings = np.take(self.wpe, range(len(code_list)), axis=0)
-        self.token_embedding_list += position_embeddings
+        final_encoding = self.wte[token_list] + self.wpe[range(len(token_list))]
 
-        # KV cache for each token for each layer
-        for token_emb in self.token_embedding_list:
-            for layer in self.layers:
-                layer.cache_kv(token_emb)
-
-    def get_next_token(self) -> str:
-        # get last item in self.token_embedding_list
-        cur_embedding = self.token_embedding_list[-1]
-
-        # do the stuf
-        for layer in self.layers:
-            cur_embedding = layer.forward(cur_embedding)
-            layer.cache_kv(cur_embedding)
+        # forawrd n layers
+        cur_embedding = final_encoding
+        for l in self.layers:
+            cur_embedding = l.forward(cur_embedding)
 
         # final norm
-        cur_embedding = (cur_embedding - np.mean(cur_embedding)) / np.std(cur_embedding)
-        cur_embedding = cur_embedding * self.ln_f_weights + self.ln_f_bias
+        output = _layer_norm(cur_embedding, self.ln_f_weights, self.ln_f_bias)
 
-        # mul with wte and get arg max
-        logits = np.dot(cur_embedding, self.wte.T)
-        output_token = np.argmax(logits)
+        # wte * output
+        output = np.dot(self.wte, output.T)
+        output = output.T[-1]
 
-        # append embedding for new token
-        new_embed = self.wte[output_token]
-        new_embed = new_embed + self.wpe[len(self.token_embedding_list)]
-        self.token_embedding_list = np.append(
-            self.token_embedding_list, [new_embed], axis=0
-        )
+        output_token = np.argmax(output)
 
-        # return token
-        return self.tokenizer.decode_single(output_token)
-
-
-# always raises some overflow error, idk why, this whole implementation is probs real messed up
-# np.seterr(all='raise')
+        # return self.tokenizer.decode_single(output_token)
+        return self.encoder.decode([output_token])
 
 model = GPT2("model")
-prompt = "The transformer architecture used in large language models is"
-model.prepare(prompt)
+
+prompt = "Alan Turing theorized that computers would one day become"
 print(prompt, end="", flush=True)
-for _ in range(10):
-    print(model.get_next_token(), end="", flush=True)
+
+for _ in range(8):
+    next_tok = model.get_next_token(prompt)
+    print(next_tok, end="", flush=True)
+    prompt += next_tok
+
 print()
